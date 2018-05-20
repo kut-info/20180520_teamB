7d435825ad47259edc8499ee241c071d6e410af1
mkohei <baberugaberiimeronnv5@gmail.com>
2018-05-20 15:36:48 +0900

    add sql

---
+++ sql/_.sql
@@ -0,0 +1,5 @@
+create database test;
+
+create table title (id int not null auto_increment, title varchar(255), primary key (id));
+
+create table time (hid int not null auto_increment, id int not null, start datetime default current_timestamp on update current_timestamp, end datetime default current_timestamp on update current_timestamp, primary key (hid));
