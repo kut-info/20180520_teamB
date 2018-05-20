e9a53f149103f9f68a9779d30dd19c0c6dd88bfe
mkohei <baberugaberiimeronnv5@gmail.com>
2018-05-20 18:12:52 +0900

    Update create sql

--- sql/_.sql
+++ sql/_.sql
@@ -2,4 +2,4 @@ create database test;
 
 create table title (id int not null auto_increment, title varchar(255), primary key (id));
 
-create table time (hid int not null auto_increment, id int not null, start datetime default current_timestamp on update current_timestamp, end datetime default current_timestamp on update current_timestamp, primary key (hid));
+create table time (hid int not null auto_increment, id int not null, start datetime default current_timestamp, end datetime default current_timestamp, primary key (hid));
